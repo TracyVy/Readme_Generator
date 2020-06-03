543c295940d15b808d25b58a2c8e3d970c24eef3
Tracy Vy <tracyvy88@gmail.com>
2020-06-02 17:01:17 -0700

    require index

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
 const axios = require("axios");
+let answers = require("./index");
 
 const api = `https://api.github.com/users/${answers.username}/repos?per_page=100`;
 
 axios.get(api).then((res) => {
-    const repoNames = res.data.map((repo) => repo.name);
-    const repoNamesStr = repoNames.join("\n");
-    console.log(`Saved ${repoNames.length} repos`);
-    console.log(`Here are the names of the repo: ${repoNamesStr}`);
+  //   console.log(res);
+  const repoNames = res.data.map((repo) => repo.name);
+  const repoNamesStr = repoNames.join("\n");
+  console.log(`Saved ${repoNames.length} repos`);
+  console.log(`Here are the names of the repo: ${repoNamesStr}`);
 
-    function generateMarkdown(res) {
-        return `
-        # ${res.title}
+  // function generateMarkdown(res) {
+  //     return `
+  //     # ${res.title}
 
-    `;
-    }
+  // `;
+  // }
 
-fs.writeFile("readme.md", res, (err) => {
-    if (err) throw err;
-},
-
+  // fs.writeFile("readme.md", res, (err) => {
+  //     if (err) throw err;
+  // },
 });
-
